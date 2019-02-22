fc11e4e28b04f7fa993b0d6a43d0dafc37604b34
AlexDor <32705886+AlexDorohoff@users.noreply.github.com>
2019-02-22 03:34:58 +0300

    обработка нажатия на тайл

--- src/components/game/game.jsx
+++ src/components/game/game.jsx
@@ -6,7 +6,16 @@ class Game extends Component {
         this.state =
             {
                 hits: this.props,
+                symb: {background: ''},
+                flag: '',
             };
+        this.onClickTile = this.onClickTile.bind(this);
+    }
+
+    onClickTile(e) {
+        this.setState({
+            flag:true
+        })
     }
 
     componentDidMount() {
@@ -18,20 +27,25 @@ class Game extends Component {
         return (
             <>
                 <div className={'login'}>
-                    <div className={'owner'}>{this.props.match.params.owner}<span className={'zero'}>&#9675;</span></div>
-                    <div className={'opponent'}><span className={'tac'}>&#10006;</span>{this.props.match.params.opponent}</div>
+                    <div className={'owner'}>
+                        <span className={'user_name'}>{this.props.match.params.owner}</span>
+                        <span className={'tic'}>&#10005;</span>
+                    </div>
+                    <div className={'opponent'}>
+                        <span className={'tac'}>&#9675;</span>
+                        <span className={'user_name'}>{this.props.match.params.opponent}</span></div>
                 </div>
                 <div className={'play_board'}>
                     <div className={'grid'}>
-                        <div className="tile"></div>
-                        <div className="tile"></div>
-                        <div className="tile"></div>
-                        <div className="tile"></div>
-                        <div className="tile"></div>
-                        <div className="tile"></div>
-                        <div className="tile"></div>
-                        <div className="tile"></div>
-                        <div className="tile"></div>
+                        <div className="tile" style={this.state.symb} backgroundImage={this.state.flag === true ? 'logo.svg': 'logo2.svg'} id={"1"} onClick={this.onClickTile}></div>
+                        <div className="tile" style={this.state.symb} id={"2"} onClick={this.onClickTile}></div>
+                        <div className="tile" style={this.state.symb} id={"3"} onClick={this.onClickTile}></div>
+                        <div className="tile" style={this.state.symb} id={"4"} onClick={this.onClickTile}></div>
+                        <div className="tile" style={this.state.symb} id={"5"} onClick={this.onClickTile}></div>
+                        <div className="tile" style={this.state.symb} id={"6"} onClick={this.onClickTile}></div>
+                        <div className="tile" style={this.state.symb} id={"7"} onClick={this.onClickTile}></div>
+                        <div className="tile" style={this.state.symb} id={"8"} onClick={this.onClickTile}></div>
+                        <div className="tile" style={this.state.symb} id={"9"} onClick={this.onClickTile}></div>
                     </div>
                 </div>
             </>
